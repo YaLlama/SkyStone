6eaa1136e3b031877e3de30d99d06cba2de9fb87
YaLlama <47946874+YaLlama@users.noreply.github.com>
2019-09-15 07:22:13 -0400

    no message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonymus.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Autonymus")
+
+public class Autonymus extends SynchronousOpMode {
+    DcMotor motorLeftFront = null;
+    DcMotor motorRightFront = null;
+    DcMotor motorRightBack = null;
+    DcMotor motorLeftBack = null;
+
+    @Override public void main() throws InterruptedException {
+
+
+
+    }
+}
