387e8a6da21117d2c884450ac9de38c0defd5a53
YaLlama <47946874+YaLlama@users.noreply.github.com>
2019-10-17 17:49:07 -0400

    Changed testing odometry class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOdometry.java
@@ -16,5 +16,28 @@ public class TestingOdometry extends LinearOpMode {
         SKRTOdometry srt = new SKRTOdometry(RFE, RBE, LFE, LB);
         waitForStart();
         srt.moveTo(45, 45, .3);
+        srt.moveTo(-90, -90, .3);
+        srt.moveTo(45, 45, .3);
+        srt.moveTo(60, 30, .3);
+        srt.moveTo(-120, -60, .3);
+        srt.moveTo(60, 30, .3);
+        srt.moveTo(30, 60, .3);
+        srt.moveTo(-60, -120, .3);
+        srt.moveTo(-45, 45, .3);
+        srt.moveTo(90, -90, .3);
+        srt.moveTo(-45,45 , .3);
+        srt.moveTo(-60, 30, .3);
+        srt.moveTo(120, -60, .3);
+        srt.moveTo(-60, 30, .3);
+        srt.moveTo(-30, 60, .3);
+        srt.moveTo(60, -120, .3);
+        srt.moveTo(-30, 60, .3);
+        srt.moveTo(100, 0, .3);
+        srt.moveTo(-200, 0, .3);
+        srt.moveTo(100, 0, .3);
+        srt.moveTo(0, 100, .3);
+        srt.moveTo(0, -200, .3);
+        srt.moveTo(0, 100, .3);
+        srt.moveTo(45, 45, .3, 90);
     }
 }
