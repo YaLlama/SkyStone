409eba0acf745b00a349ef024651adee0a4d0cb1
YaLlama <47946874+YaLlama@users.noreply.github.com>
2019-10-29 16:03:57 -0400

    Starting teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extrusion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extrusion.java
@@ -18,6 +18,14 @@ public class Extrusion {
     //joystick dead zone
     static final double DEAD_ZONE = 0;
 
+    //servo max and min
+    static final double MAX_ROTATION = 0;
+    static final double MINIMUM_ROTATION = 0;
+
+    //open position of clamping servo
+    static final double OPEN_POSSITION = 0;
+
+
     private static DcMotor leftFront;
     private static DcMotor rightFront;
     private static DcMotor leftBack;
@@ -31,11 +39,20 @@ public class Extrusion {
     private boolean extrusionAutonymus = false;
     private boolean placingAutonymus = false;
 
+    private boolean step1done = false;
+    private boolean step2done = false;
+
+
     //encoder value with first block placed perfectly on build plate
     public static final int FIRST_LEVEL_HEIGHT = 0;
 
     //amount of encoder value needed to be added to clear block trying to stack on
     public static final int CLEARENCE = 0;
+
+    //the encoder amount it needs to be lowered in order to lock block into place
+    public static final int LOCKED_POSITION = 0;
+
+
 
     //driving
     final int s1 = 1;
@@ -48,7 +65,7 @@ public class Extrusion {
     double x2;
 
 
-    public void init(DcMotor extrusion, Gamepad gamepad1, Gamepad gamePad2, DcMotor lf, DcMotor lb, DcMotor rb, DcMotor rf, Servo clampServo, Servo rotateServo){
+    public Extrusion(DcMotor extrusion, Gamepad gamepad1, Gamepad gamePad2, DcMotor lf, DcMotor lb, DcMotor rb, DcMotor rf, Servo clampServo, Servo rotateServo){
         ex = extrusion;
         g2 = gamePad2;
         g1 = gamepad1;
@@ -106,38 +123,68 @@ public class Extrusion {
             ex.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             extrusionAutonymus = false;
             ex.setPower(EXTRUSION_POWER);
-        }else if(g2.dpad_down && ex.getCurrentPosition() > 0){
+        }else if(g2.dpad_down && ex.getCurrentPosition() > 0) {
             ex.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             extrusionAutonymus = false;
             ex.setPower(-EXTRUSION_POWER);
-
+        }
     }
 
     public void extrusionAuto(){
         //control for locking to closest height
-        if(g2.x){
+        if(g2.x) {
             extrusionAutonymus = true;
             ex.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             //snapping
             ex.setTargetPosition((ex.getCurrentPosition() - FIRST_LEVEL_HEIGHT - BOTTOM_SNAP) / CLEARENCE + FIRST_LEVEL_HEIGHT);
             ex.setPower(EXTRUSION_POWER);
-
+        }
     }
 
     public void placeBlockManual(){
         //Control for moving clamping servos
-        if(g2.left_stick_x > DEAD_ZONE && ex.getCurrentPosition() < MAX_HEIGHT){
-            extrusionAutonymus = false;
-            ex.setPower(EXTRUSION_POWER);
-
+        if(Math.abs(g2.left_stick_x) > DEAD_ZONE && rs.getPosition() < MAX_ROTATION && rs.getPosition() > MINIMUM_ROTATION) {
+            placingAutonymus = false;
+            rs.setPosition(rs.getPosition() + g2.left_stick_y * .1);
+        }
     }
 
     public void placeBlockAuto(){
+        //controls for automatically placing block, need to hold x
+        step1done = false;
+        step2done = false;
+        while(g1.x){
+            if(!step1done) {
+                rs.setPosition(MAX_ROTATION);
+                if (rs.getPosition() == MAX_ROTATION) {
+                    ex.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    ex.setTargetPosition(ex.getCurrentPosition() - LOCKED_POSITION);
+                    step1done = true;
+                }
+            }
+            else if(!step2done) {
+                if (ex.getCurrentPosition() == ex.getTargetPosition()) {
+                    cs.setPosition(OPEN_POSSITION);
+                    if(cs.getPosition() == OPEN_POSSITION){
+                        ex.setTargetPosition(ex.getCurrentPosition() + LOCKED_POSITION);
+                        step2done = true;
+                    }
+                }
+            }else{
+                if(ex.getCurrentPosition() == ex.getTargetPosition()){
+                    break;
+                }
+            }
 
+        }
     }
 
     public void reset(){
-
+        if(g1.a) {
+            ex.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            ex.setTargetPosition(0);
+            rs.setPosition(MINIMUM_ROTATION);
+        }
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class TeleOp extends OpMode {
 
@@ -11,12 +12,24 @@ public class TeleOp extends OpMode {
     DcMotor rightBack;
     DcMotor extusion;
 
+    Servo clampServo;
+    Servo rotationServo;
+
     Extrusion ex;
 
     @Override
     public void init() {
-        ex = new Extrusion();
-        ex.init(extusion, gamepad2);
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
+        extusion = hardwareMap.dcMotor.get("extrusion");
+
+        clampServo = hardwareMap.servo.get("clamp");
+        rotationServo = hardwareMap.servo.get("rotation");
+
+        ex = new Extrusion(extusion, gamepad1, gamepad2, leftFront, leftBack, rightBack, rightFront, clampServo, rotationServo);
+
     }
 
     @Override
